3b996965b17888969540ead9ac810dd5c84e947d
Maxim Paymushkin <maxim.paymushkin@gmail.com>
2019-05-24 01:40:00 +0300

    use temp dir instead of /tmp
    Signed-off-by: Maxim Paymushkin <maxim.paymushkin@gmail.com>

--- builder.sh
+++ builder.sh
@@ -3,18 +3,25 @@
 # Getting the user id.
 MYUID=`id -u`
 
+TMP_INSTALL_DIR=`mktemp -d`
+if [ ! -d $TMP_INSTALL_DIR ]
+then
+	echo "Create temp dir failed"
+	exit 1
+fi
+
 # Copying the Dockerfile to set parameters.
-cp -f Dockerfile /tmp/ && cp -f finalize_installation.sh /tmp/
+cp -f Dockerfile $TMP_INSTALL_DIR && cp -f finalize_installation.sh $TMP_INSTALL_DIR
 
 # Setting the right user id.
-sed -i -e "s/1001/$MYUID/g" /tmp/Dockerfile
+sed -i -e "s/1001/$MYUID/g" $TMP_INSTALL_DIR/Dockerfile
 
 # Stripping the escapements of the end of lines to allow incremental build of the docker image (build testing vs size optimization).
-#sed -i -e 's/ && \\//g' /tmp/Dockerfile
-#sed -i -e 's/^	/RUN /g' /tmp/Dockerfile
+#sed -i -e 's/ && \\//g' $TMP_INSTALL_DIR/Dockerfile
+#sed -i -e 's/^	/RUN /g' $TMP_INSTALL_DIR/Dockerfile
 
 # Building the image.
-sudo docker build -t webanck/docker-wine-steam /tmp/ 
+sudo docker build --no-cache -t webanck/docker-wine-steam $TMP_INSTALL_DIR
 
 # Cleaning up.
-rm /tmp/Dockerfile /tmp/finalize_installation.sh
+rm -fr $TMP_INSTALL_DIR
